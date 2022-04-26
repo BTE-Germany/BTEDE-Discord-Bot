d26e704e31d79d2aaff7efdbed1d47847bd520bc
Mic. R <Michael.reimchen@outlook.com>
2022-04-26 20:35:12 +0200

    Update duty.js

--- src/commands/team/duty.js
+++ src/commands/team/duty.js
@@ -1,44 +1,43 @@
 const {
-    MessageEmbed,
-    Interaction,
-    Client,
-    CommandInteraction,
+  MessageEmbed,
+  Interaction,
+  Client,
+  CommandInteraction,
 } = require("discord.js");
 const Command = require("../../classes/Command.js");
 const Bot = require("../../classes/Bot.js");
 
 class WarnCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: "duty",
-            description: "Get a support duty.",
-            userAvailable: false
+  constructor(client) {
+    super(client, {
+      name: "duty",
+      description: "Get a support duty.",
+      userAvailable: false,
+    });
+  }
 
-        });
+  /**
+   *
+   * @param {CommandInteraction} interaction
+   * @param {Bot} client
+   */
+
+  async run(interaction, client) {
+    const user = interaction.member;
+    const guild = client.guilds.cache.get(client.config.guild);
+    const member = guild.members.cache.get(user.id);
+
+    if (!member.roles.cache.get(client.config.roles.support))
+      return this.error("You are not a support member! You can't get on duty!");
+
+    if (member.roles.cache.has(client.config.roles.duty)) {
+      await member.roles.remove(client.config.roles.duty);
+      return this.response(interaction, "You are now __not__ on duty!");
+    } else {
+      await member.roles.add(client.config.roles.duty);
+      return this.response(interaction, "You are now __on duty__!");
     }
-
-    /**
-     *
-     * @param {CommandInteraction} interaction
-     * @param {Bot} client
-     */
-
-    async run(interaction, client) {
-        
-const user = interaction.member;
-const guild = client.guilds.cache.get(client.config.guild);
-const member = guild.members.cache.get(user.id);
-
-if(!member.roles.cache.get(client.config.roles.support)) return this.error("You are not a support member! You can't get on duty!");
-
-if(member.roles.cache.has(client.config.roles.duty)) {
-  await member.roles.remove(client.config.roles.duty);
-  return this.response(interaction, "You are now __not__ on duty!");
-} else {
-  await member.roles.add(client.config.roles.duty);
-  return this.response(interaction, "You are now __on duty__!");
-}
-
+  }
 }
 
 module.exports = WarnCommand;
