00a3fd6377fd99cad295913a7023c09b5cfc7ee4
Michael "Mischa" Alexandrowitsch R <michael@mic-r.eu>
2023-03-06 20:22:07 +0100

    Update citamehcs.js

--- src/commands/team/citamehcs.js
+++ src/commands/team/citamehcs.js
@@ -1,54 +1,45 @@
-const { CommandInteraction, MessageAttachment } = require("discord.js");
+const {CommandInteraction, MessageAttachment} = require("discord.js");
 const Command = require("../../classes/Command.js");
 const axios = require("axios");
 const Bot = require("../../classes/Bot.js");
 
 class citamehcsCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: "citamehcs",
-      description: "Downloads a schematic",
-      userAvailable: false,
-      options: [
-        {
-              name: "schematic",
-              description: "The schematic to download",
-              type: 3,
-              required: true
-            },
-            {
-              name: "terra",
-              description: "The terraserver to upload to",
-              type: 3,
-              required: true,
-              choices: client.config.status.map((s) => ({ name: s.id, value: s.id })),
-            }
-      ],
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: "citamehcs", description: "Downloads a schematic", userAvailable: false, options: [{
+                name: "schematic", description: "The schematic to download", type: 3, required: true
+            }, {
+                name: "terra",
+                description: "The terraserver to upload to",
+                type: 3,
+                required: true,
+                choices: client.config.status.map((s) => ({name: s.id, value: s.id})),
+            }],
+        });
+    }
 
-  /**
-   *
-   * @param {CommandInteraction} interaction
-   * @param {Bot} client
-   */
+    /**
+     *
+     * @param {CommandInteraction} interaction
+     * @param {Bot} client
+     */
 
-  async run(interaction, client) {
-    const terraname = await interaction.options._hoistedOptions.find((x) => x.name === "terra").value;
-    const name = await interaction.options._hoistedOptions.find((x) => x.name === "schematic").value;
+    async run(interaction, client) {
+        const terraname = await interaction.options._hoistedOptions.find((x) => x.name === "terra").value;
+        const name = await interaction.options._hoistedOptions.find((x) => x.name === "schematic").value;
 
-    await axios.get(`http://cloud.bte.ger:45655/api/schematics/download?terra=${terraname.replace(" ", "-")}&name=${name}`, { responseType: "arraybuffer"})
-      .then(async ({data: schem}) => {
-        console.log(schem)
-        const attachment = new MessageAttachment(schem, name+'.schematic');
-        await interaction.editReply("!og uoy ereH");
-        return client.channels.cache.get(interaction.channelId).send({content: null, files: [attachment]});
-      })
-      .catch(async (e) => {
-        await this.error(interaction, "Schematic not found");
-        return console.log(e);
-      })
-  }
+        await axios.get(`http://cloud.bte.ger:45655/api/schematics/download?terra=${terraname.replace(" ", "-")}&name=${name}`, {responseType: "arraybuffer"})
+            .then(async ({data: schem}) => {
+                console.log(schem)
+                const attachment = new MessageAttachment(schem, name + '.schematic');
+                await interaction.editReply("!og uoy ereH");
+                return client.channels.cache.get(interaction.channelId).send({content: null, files: [attachment]});
+            })
+            .catch(async (e) => {
+                await this.error(interaction, "Schematic not found");
+                return console.log(e);
+            })
+    }
 }
 
 module.exports = citamehcsCommand;
