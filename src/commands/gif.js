59b2d334521a2c823cc3fa1a3b1db7d573fe9bc1
Simon Reinsch <gruenfink1111@googlemail.com>
2025-01-21 23:56:04 +0100

    remove "/gif ram" command

--- src/commands/gif.js
+++ src/commands/gif.js
@@ -60,8 +60,6 @@ module.exports = class extends Command {
       gifLink = "https://imgur.com/9Arqjau";
     } else if (gif === "measure") {
       gifLink = "https://gyazo.com/d58446cec35cc504bb36b749346041a9";
-    } else if (gif === "ram") {
-      gifLink = "https://media.nachwahl.dev/FmtvIo.gif";
     }
 
     return await this.response(interaction, gifLink);
