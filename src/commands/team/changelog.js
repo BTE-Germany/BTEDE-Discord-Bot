8ef9e93bea8329cee679ef59629e9fd1ec856ad3
thereal_elias <eliasmaerz1000@gmx.de>
2025-01-27 20:25:14 +0100

    Update changelog.js

--- src/commands/team/changelog.js
+++ src/commands/team/changelog.js
@@ -1,1 +1,69 @@
+const { 
+  SlashCommandBuilder, 
+  ActionRowBuilder, 
+  ModalBuilder, 
+  TextInputBuilder, 
+  TextInputStyle 
+} = require('discord.js');
 
+module.exports = {
+	admin: true,
+	data: new SlashCommandBuilder()
+		.setName('changelog')
+		.setDescription('Öffnet ein Eingabefeld, um einen Changelog zu senden.'),
+	async execute(interaction, client) {
+		const modal = new ModalBuilder()
+			.setTitle('Changelog erstellen')
+			.setCustomId('changelog');
+
+		const titleInput = new TextInputBuilder()
+			.setCustomId('title')
+			.setPlaceholder('Titel des Embeds')
+			.setLabel('Titel')
+			.setStyle(TextInputStyle.Short)
+			.setMaxLength(200)
+			.setRequired(true); 
+
+		const staffInput = new TextInputBuilder()
+			.setCustomId('staff')
+			.setPlaceholder('Staff Änderungen (z. B. Teamänderungen, wichtige Infos)')
+			.setLabel('Staff')
+			.setStyle(TextInputStyle.Paragraph)
+			.setMaxLength(450)
+			.setRequired(false); 
+
+		const changesInput = new TextInputBuilder()
+			.setCustomId('changes')
+			.setPlaceholder('Änderungen auf Discord oder Minecraft (kombiniert)')
+			.setLabel('Discord & Minecraft Changes')
+			.setStyle(TextInputStyle.Paragraph)
+			.setMaxLength(450)
+			.setRequired(false); 
+
+		const protocolsInput = new TextInputBuilder()
+			.setCustomId('meetingProtocols')
+			.setPlaceholder('Zusammenfassung von Meetings')
+			.setLabel('Meeting Protocols')
+			.setStyle(TextInputStyle.Paragraph)
+			.setMaxLength(450)
+			.setRequired(false); 
+
+		const generalInput = new TextInputBuilder()
+			.setCustomId('general')
+			.setPlaceholder('Allgemeine Informationen oder Spenden-Infos')
+			.setLabel('General & Donations')
+			.setStyle(TextInputStyle.Paragraph)
+			.setMaxLength(450)
+			.setRequired(false); 
+
+		const titleRow = new ActionRowBuilder().addComponents(titleInput);
+		const staffRow = new ActionRowBuilder().addComponents(staffInput);
+		const changesRow = new ActionRowBuilder().addComponents(changesInput);
+		const protocolsRow = new ActionRowBuilder().addComponents(protocolsInput);
+		const generalRow = new ActionRowBuilder().addComponents(generalInput);
+
+		modal.addComponents(titleRow, staffRow, changesRow, protocolsRow, generalRow);
+
+		await interaction.showModal(modal);
+	},
+};
