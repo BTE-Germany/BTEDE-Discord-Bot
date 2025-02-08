f5b8f2063e7656a1348a58d16ec80b9641e5541c
thereal_elias <eliasmaerz1000@gmx.de>
2025-02-08 22:26:49 +0100

    Update changelog.js

--- src/commands/team/changelog.js
+++ src/commands/team/changelog.js
@@ -1,11 +1,7 @@
-const { 
-  MessageEmbed,
-  Interaction,
-  Client,
-  CommandInteraction,
-  ActionRowBuilder, 
-  ModalBuilder, 
-  TextInputBuilder, 
+const {
+  ActionRowBuilder,
+  ModalBuilder,
+  TextInputBuilder,
   TextInputStyle,
 } = require("discord.js");
 const Command = require("../../classes/Command.js");
@@ -29,6 +25,7 @@ module.exports = class extends Command {
       .setTitle("Changelog erstellen")
       .setCustomId("changelog");
 
+    // Eingabefeld für den Titel des Changelogs
     const titleInput = new TextInputBuilder()
       .setCustomId("title")
       .setPlaceholder("Titel des Embeds")
@@ -37,6 +34,7 @@ module.exports = class extends Command {
       .setMaxLength(200)
       .setRequired(true);
 
+    // Eingabefeld für Änderungen des Staffs
     const staffInput = new TextInputBuilder()
       .setCustomId("staff")
       .setPlaceholder("Staff Änderungen (z. B. Teamänderungen, wichtige Infos)")
@@ -45,6 +43,7 @@ module.exports = class extends Command {
       .setMaxLength(450)
       .setRequired(false);
 
+    // Eingabefeld für Änderungen auf Discord oder Minecraft
     const changesInput = new TextInputBuilder()
       .setCustomId("changes")
       .setPlaceholder("Änderungen auf Discord oder Minecraft (kombiniert)")
@@ -53,6 +52,7 @@ module.exports = class extends Command {
       .setMaxLength(450)
       .setRequired(false);
 
+    // Eingabefeld für Meeting-Protokolle
     const protocolsInput = new TextInputBuilder()
       .setCustomId("meetingProtocols")
       .setPlaceholder("Zusammenfassung von Meetings")
@@ -61,6 +61,7 @@ module.exports = class extends Command {
       .setMaxLength(450)
       .setRequired(false);
 
+    // Eingabefeld für allgemeine Informationen oder Spenden-Infos
     const generalInput = new TextInputBuilder()
       .setCustomId("general")
       .setPlaceholder("Allgemeine Informationen oder Spenden-Infos")
@@ -69,14 +70,17 @@ module.exports = class extends Command {
       .setMaxLength(450)
       .setRequired(false);
 
+    // Action Rows für jedes Eingabefeld
     const titleRow = new ActionRowBuilder().addComponents(titleInput);
     const staffRow = new ActionRowBuilder().addComponents(staffInput);
     const changesRow = new ActionRowBuilder().addComponents(changesInput);
     const protocolsRow = new ActionRowBuilder().addComponents(protocolsInput);
     const generalRow = new ActionRowBuilder().addComponents(generalInput);
 
+    // Hinzufügen der Action Rows zum Modal
     modal.addComponents(titleRow, staffRow, changesRow, protocolsRow, generalRow);
 
+    // Modal anzeigen
     await interaction.showModal(modal);
   }
 };
