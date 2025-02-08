38d452059160a987fee73697e27c80a50d6dee33
thereal_elias <eliasmaerz1000@gmx.de>
2025-02-08 23:08:43 +0100

    Update changelog.js

--- src/commands/team/changelog.js
+++ src/commands/team/changelog.js
@@ -25,7 +25,6 @@ module.exports = class extends Command {
       .setTitle("Changelog erstellen")
       .setCustomId("changelog");
 
-    // Eingabefeld für den Titel des Changelogs
     const titleInput = new TextInputBuilder()
       .setCustomId("title")
       .setPlaceholder("Titel des Embeds")
@@ -34,7 +33,6 @@ module.exports = class extends Command {
       .setMaxLength(200)
       .setRequired(true);
 
-    // Eingabefeld für Änderungen des Staffs
     const staffInput = new TextInputBuilder()
       .setCustomId("staff")
       .setPlaceholder("Staff Änderungen (z. B. Teamänderungen, wichtige Infos)")
@@ -43,7 +41,6 @@ module.exports = class extends Command {
       .setMaxLength(450)
       .setRequired(false);
 
-    // Eingabefeld für Änderungen auf Discord oder Minecraft
     const changesInput = new TextInputBuilder()
       .setCustomId("changes")
       .setPlaceholder("Änderungen auf Discord oder Minecraft (kombiniert)")
@@ -52,7 +49,6 @@ module.exports = class extends Command {
       .setMaxLength(450)
       .setRequired(false);
 
-    // Eingabefeld für Meeting-Protokolle
     const protocolsInput = new TextInputBuilder()
       .setCustomId("meetingProtocols")
       .setPlaceholder("Zusammenfassung von Meetings")
@@ -61,7 +57,6 @@ module.exports = class extends Command {
       .setMaxLength(450)
       .setRequired(false);
 
-    // Eingabefeld für allgemeine Informationen oder Spenden-Infos
     const generalInput = new TextInputBuilder()
       .setCustomId("general")
       .setPlaceholder("Allgemeine Informationen oder Spenden-Infos")
@@ -70,17 +65,14 @@ module.exports = class extends Command {
       .setMaxLength(450)
       .setRequired(false);
 
-    // Action Rows für jedes Eingabefeld
     const titleRow = new ActionRowBuilder().addComponents(titleInput);
     const staffRow = new ActionRowBuilder().addComponents(staffInput);
     const changesRow = new ActionRowBuilder().addComponents(changesInput);
     const protocolsRow = new ActionRowBuilder().addComponents(protocolsInput);
     const generalRow = new ActionRowBuilder().addComponents(generalInput);
 
-    // Hinzufügen der Action Rows zum Modal
     modal.addComponents(titleRow, staffRow, changesRow, protocolsRow, generalRow);
 
-    // Modal anzeigen
     await interaction.showModal(modal);
   }
 };
