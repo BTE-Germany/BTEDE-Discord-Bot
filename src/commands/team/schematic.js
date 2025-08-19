dc3dbf4fdf421b924838f39c0ccf5132d4ac0f39
Michael Reimchen <michael@mic-r.eu>
2025-08-19 19:07:05 +0200

    Update schematic.js

--- src/commands/team/schematic.js
+++ src/commands/team/schematic.js
@@ -14,33 +14,15 @@ class schematicCommand extends Command {
                     type: 11,
                     required: true
                 }, {
-                    name: "terra",
-                    description: "The terraserver to upload to",
-                    type: 3,
-                    required: true,
-                    choices: client.config.status.map((s) => ({name: s.id, value: s.id})),
-                }, {
                     name: "comment",
                     description: "A comment you want to add to the message",
                     type: 3,
                     required: false
                 }]
             }, {
-                name: "list", description: "Lists all schematics on the given server", type: 1, options: [{
-                    name: "terra",
-                    description: "The terraserver to list schematics from",
-                    type: 3,
-                    required: true,
-                    choices: client.config.status.map((s) => ({name: s.id, value: s.id})),
-                }]
+                name: "list", description: "Lists all schematics on the given server", type: 1
             }, {
                 name: "download", description: "Downloads a schematic", type: 1, options: [{
-                    name: "terra",
-                    description: "The terraserver to download from",
-                    type: 3,
-                    required: true,
-                    choices: client.config.status.map((s) => ({name: s.id, value: s.id})),
-                }, {
                     name: "schematic", description: "The schematic to download", type: 3, required: true
                 }, {
                     name: "comment",
@@ -49,34 +31,7 @@ class schematicCommand extends Command {
                     required: false
                 }]
             }, {
-                name: "transfer", description: "Transfers a schematic", type: 1, options: [{
-                    name: "fromserver",
-                    description: "The terraserver to transfer from",
-                    type: 3,
-                    required: true,
-                    choices: client.config.status.map((s) => ({name: s.id, value: s.id})),
-                }, {
-                    name: "toserver",
-                    description: "The terraserver to transfer to",
-                    type: 3,
-                    required: true,
-                    choices: client.config.status.map((s) => ({name: s.id, value: s.id})),
-                }, {
-                    name: "schematic", description: "The schematic to transfer", type: 3, required: true
-                }, {
-                    name: "comment",
-                    description: "A comment you want to add to the message",
-                    type: 3,
-                    required: false
-                }]
-            }, {
                 name: "delete", description: "Deletes a schematic", type: 1, options: [{
-                    name: "terra",
-                    description: "The terraserver to delete from",
-                    type: 3,
-                    required: true,
-                    choices: client.config.status.map((s) => ({name: s.id, value: s.id})),
-                }, {
                     name: "schematic", description: "The schematic to delete", type: 3, required: true
                 }, {
                     name: "comment",
@@ -114,24 +69,22 @@ class schematicCommand extends Command {
           }
 
         if (interaction.options.getSubcommand() === "list") {
-            const terraname = await interaction.options._hoistedOptions.find((x) => x.name === "terra").value || "";
-            await axios.get(`http://cloud.bte.ger:45655/api/schematics/list?terra=${terraname.replace(" ", "-")}`).then((res) => {
+            await axios.get(`http://cloud.bte.ger:45655/api/schematics/list`).then((res) => {
                 let dat = res.data
                     .filter(a => a.endsWith(".schematic") || a.endsWith(".schem"))
                     .map(a => a.replace(".schematic", "").replace(".schem", ""));
                 let chunks = chunkify(dat, 1950);
-                this.response(interaction, `Schematics on ${terraname}:`);
+                this.response(interaction, `Schematics on the server:`);
                 chunks.forEach(chunk => {
                     return client.channels.cache.get(interaction.channelId).send({content: "```" + chunk + "```"});
                 });
             }).catch((e) => {
                 console.log(e.message);
-                return this.error(interaction, `Failed to list schematics on ${terraname}!`);
+                return this.error(interaction, `Failed to list schematics on the server!`);
             })
         }
 
         if (interaction.options.getSubcommand() === "upload") {
-            const terraname = await interaction.options._hoistedOptions.find((x) => x.name === "terra").value || "";
             const schemfile = await interaction.options._hoistedOptions.find((o) => o.name === "schematic").attachment;
             let comment = "";
 
@@ -139,26 +92,25 @@ class schematicCommand extends Command {
             if (!schemfile) return this.error(interaction, "Please provide a valid schematic!");
             if (!(schemfile.name.endsWith(".schematic") || schemfile.name.endsWith(".schem"))) return this.error(interaction, "Please provide the schematic in the form of a schematic!");
             await axios.post("http://cloud.bte.ger:45655/api/schematics/upload", {
-                "url": schemfile.url, "terra": terraname.replace(" ", "-"),
+                "url": schemfile.url,
             }).then((res) => {
-                this.response(interaction, `Successfully uploaded the schematic to ${res.data.server} as ${res.data.fileName}.schematic`);
+                this.response(interaction, `Successfully uploaded the schematic to the server as ${res.data.fileName}.schematic`);
                 if(comment.length > 0 ) client.channels.cache.get(interaction.channelId).send({content: "Kommentar: " + comment});
                 return;
             }).catch((e) => {
                 console.log(e.message);
-                return this.error(interaction, `Failed to upload the schematic to ${terraname}!`);
+                return this.error(interaction, `Failed to upload the schematic to the server!`);
             })
         }
 
         if (interaction.options.getSubcommand() === "download") {
-            const terraname = await interaction.options._hoistedOptions.find((x) => x.name === "terra").value || "";
             const name = await interaction.options._hoistedOptions.find((x) => x.name === "schematic").value;
             let comment = "";
 
             try { comment = await interaction.options._hoistedOptions.find((x) => x.name === "comment").value; } catch {}
-            await axios.get(`http://cloud.bte.ger:45655/api/schematics/download?terra=${terraname.replace(" ", "-")}&name=${name}`, {responseType: "arraybuffer"})
+            await axios.get(`http://cloud.bte.ger:45655/api/schematics/download?name=${name}`, {responseType: "arraybuffer"})
                 .then(async ({data: schem}) => {
-                    let {data: filetype} = await axios.get(`http://cloud.bte.ger:45655/api/schematics/filetype?terra=${terraname.replace(" ", "-")}&name=${name}`);
+                    let {data: filetype} = await axios.get(`http://cloud.bte.ger:45655/api/schematics/filetype?name=${name}`);
                     const attachment = new MessageAttachment(schem, name + filetype);
                     await interaction.editReply("Here you go!");
                     return client.channels.cache.get(interaction.channelId).send({content: "Kommentar: " + comment, files: [attachment]});
@@ -168,36 +120,13 @@ class schematicCommand extends Command {
                     return console.log(e);
                 });
         }
-
-        if (interaction.options.getSubcommand() === "transfer") {
-            const terra1 = await interaction.options._hoistedOptions.find((x) => x.name === "fromserver").value;
-            const terra2 = await interaction.options._hoistedOptions.find((x) => x.name === "toserver").value;
-            const name = await interaction.options._hoistedOptions.find((x) => x.name === "schematic").value;
-            let comment = "";
-
-            try { comment = await interaction.options._hoistedOptions.find((x) => x.name === "comment").value; } catch {}
-            await axios.post("http://cloud.bte.ger:45655/api/schematics/transfer", {
-                "terra1": terra1,
-                "terra2": terra2,
-                "name": name
-            }).then((res) => {
-                this.response(interaction, res.data.message);
-                if(comment.length > 0 ) client.channels.cache.get(interaction.channelId).send({content: "Kommentar: " + comment});
-                return;
-            }).catch((e) => {
-                console.log(e.message);
-                return this.error(interaction, `Failed to transfer the schematic from ${terra1} to ${terra2}!`);
-            })
-        }
         
         if (interaction.options.getSubcommand() === "delete") {
-            const terra = await interaction.options._hoistedOptions.find((x) => x.name === "terra").value;
             const name = await interaction.options._hoistedOptions.find((x) => x.name === "schematic").value;
             let comment = "";
 
             try { comment = await interaction.options._hoistedOptions.find((x) => x.name === "comment").value; } catch {}
             await axios.post("http://cloud.bte.ger:45655/api/schematics/transfer", {
-                "terra": terra,
                 "name": name
             }).then((res) => {
                 this.response(interaction, res.data.message);
@@ -205,7 +134,7 @@ class schematicCommand extends Command {
                 return;
             }).catch((e) => {
                 console.log(e.message);
-                return this.error(interaction, `Failed to delete the schematic from ${terra}!`);
+                return this.error(interaction, `Failed to delete the schematic from the server!`);
             })
         }
     }
